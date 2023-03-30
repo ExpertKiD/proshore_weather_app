0fe76a2a74c45b75e347f2076a64e11a22d1a5d6
Suman Adhikari <expertxkid@gmail.com>
2023-03-30 16:54:53 +0545

    refactor/PWA-1: add color palette named AppPalette in palettes.dart

---
+++ lib/core/palettes/palettes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppPalette {
+  static Color white = Colors.white;
+  static Color grey =const Color(0xffcccccc);
+  static Color grey700 = const Color(0xff646464);
+  static Color black = Colors.black;
+  static Color sunshineLightBlue = const Color(0xff64c2f4);
+  static Color sunshineBlue = const Color(0xff1ca8f4);
+}
