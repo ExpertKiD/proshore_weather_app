9a1345baa20f7c9887be8c08f56d1e5ebad54080
Suman Adhikari <expertxkid@gmail.com>
2023-03-30 16:28:34 +0545

    refactor/PWA-1: add assets.dart

---
+++ lib/core/helpers/assets.dart
@@ -0,0 +1,22 @@
+abstract class WeatherArtworkAssets {
+  static const String clear = 'assets/images/artworks/art_clear.png';
+  static const String clouds = 'assets/images/artworks/art_clouds.png';
+  static const String fog = 'assets/images/artworks/art_fog.png';
+  static const String lightClouds = 'assets/images/artworks/art_light_clouds.png';
+  static const String lightRain = 'assets/images/artworks/art_light_rain.png';
+  static const String rain = 'assets/images/artworks/art_rain.png';
+  static const String snow = 'assets/images/artworks/art_snow.png';
+  static const String storm = 'assets/images/artworks/art_storm.png';
+}
+
+abstract class IconAssets {
+  static const String clear = 'assets/images/artworks/ic_clear.png';
+  static const String clouds = 'assets/images/artworks/ic_clouds.png';
+  static const String fog = 'assets/images/artworks/ic_fog.png';
+  static const String lightClouds = 'assets/images/artworks/ic_light_clouds.png';
+  static const String lightRain = 'assets/images/artworks/ic_light_rain.png';
+  static const String rain = 'assets/images/artworks/ic_rain.png';
+  static const String snow = 'assets/images/artworks/ic_snow.png';
+  static const String storm = 'assets/images/artworks/ic_storm.png';
+  static const String logo = 'assets/images/artworks/ic_logo.png';
+}
