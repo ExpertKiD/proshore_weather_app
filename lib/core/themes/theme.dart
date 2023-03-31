5dd5763061ea771c7fa833b95fd1dbffdff904d5
Suman Adhikari <expertxkid@gmail.com>
2023-03-31 10:49:17 +0545

    refactor/PWA-1: update status bar color in theme_light.dart

--- lib/core/themes/theme.dart
+++ lib/core/themes/theme.dart
@@ -1,10 +1,12 @@
 import 'package:app/core/palettes/palettes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 part 'theme_dark.dart';
 part 'theme_light.dart';
 
-class ThemeManager{
+class ThemeManager {
   static get light => _lightTheme;
+
   static get dark => _darkTheme;
 }
--- lib/core/themes/theme_light.dart
+++ lib/core/themes/theme_light.dart
@@ -2,8 +2,10 @@ part of 'theme.dart';
 
 get _lightTheme => ThemeData(
       brightness: Brightness.light,
-
-  appBarTheme: AppBarTheme(
-    color: AppPalette.sunshineBlue,
-  ),
+      appBarTheme: AppBarTheme(
+        color: AppPalette.sunshineBlue,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: AppPalette.sunshineLightBlue,
+        ),
+      ),
     );
