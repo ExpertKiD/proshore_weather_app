9ae0ba8a8b00ad4c8f0ec2819a7abb3147502296
Suman Adhikari <expertxkid@gmail.com>
2023-03-31 10:53:14 +0545

    refactor/PWA-1: create general forecast widgets in home_mobile.dart

--- lib/features/home/presentation/widgets/pages/home.dart
+++ lib/features/home/presentation/widgets/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:app/core/helpers/assets.dart';
+import 'package:app/core/palettes/palettes.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 part 'home_mobile.dart';
