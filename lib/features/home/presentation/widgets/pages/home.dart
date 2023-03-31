7506d0bcf1ab6dd05c8493b597c8bfc802744745
Suman Adhikari <expertxkid@gmail.com>
2023-03-31 11:10:03 +0545

    feat/PWA-1: update HomeMobileWidget to show static tiles and banner

--- lib/features/home/presentation/widgets/pages/home.dart
+++ lib/features/home/presentation/widgets/pages/home.dart
@@ -1,9 +1,11 @@
 import 'package:app/core/helpers/assets.dart';
-import 'package:app/core/palettes/palettes.dart';
+import 'package:app/features/home/presentation/widgets/atoms/forecasts/forecast_tile.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+
+import '../atoms/forecasts/forecast_banner.dart';
 
 part 'home_mobile.dart';
 part 'home_tablet.dart';
--- lib/features/home/presentation/widgets/pages/home_mobile.dart
+++ lib/features/home/presentation/widgets/pages/home_mobile.dart
@@ -5,6 +5,17 @@ class HomeMobileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const List<Widget> mWeatherForecasts = <Widget>[
+      WeatherForecastBanner(),
+      WeatherForecastTile(),
+      WeatherForecastTile(),
+      WeatherForecastTile(),
+      WeatherForecastTile(),
+      WeatherForecastTile(),
+      WeatherForecastTile(),
+      WeatherForecastTile(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -31,145 +42,11 @@ class HomeMobileWidget extends StatelessWidget {
           )
         ],
       ),
-      body: ListView(
-        children: [
-          Container(
-            color: AppPalette.sunshineBlue,
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 24),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Today, March 30',
-                          style: GoogleFonts.robotoCondensed(
-                            fontSize: 18,
-                            color: Colors.white,
-                          ),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        Text(
-                          '25\u00b0',
-                          style: GoogleFonts.roboto(
-                            fontSize: 72,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          '15\u00b0',
-                          style: GoogleFonts.roboto(
-                            fontSize: 36,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        WeatherArtworkAssets.rain,
-                        width: 150,
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Rain',
-                        style: GoogleFonts.roboto(
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          InkWell(
-            onTap: () {
-              // TODO: Show detail page if needed
-            },
-            highlightColor: AppPalette.sunshineLightBlue,
-            child: Container(
-              // color: AppPalette.sunshineLightBlue,
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-              child: Row(
-                children: [
-                  Container(
-                    width: 45,
-                    height: 45,
-                    padding: const EdgeInsets.only(
-                      right: 16,
-                      top: 0,
-                      bottom: 0,
-                    ),
-                    child: Image.asset(
-                      IconAssets.cloudy,
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Tomorrow',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: GoogleFonts.robotoCondensed(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w200,
-                            color: AppPalette.grey700,
-                          ),
-                        ),
-                        Text(
-                          'Rain',
-                          style: GoogleFonts.robotoCondensed(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w200,
-                            color: AppPalette.grey700,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        '24\u00b0',
-                        style: GoogleFonts.roboto(
-                          fontSize: 24,
-                        ),
-                      ),
-                      Text(
-                        '15\u00b0',
-                        style: GoogleFonts.roboto(
-                          fontSize: 16,
-                          color: AppPalette.grey700,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+      body: ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (context, index) => mWeatherForecasts[index],
+        separatorBuilder: (context, index) => const SizedBox.shrink(),
+        itemCount: mWeatherForecasts.length,
       ),
     );
   }
