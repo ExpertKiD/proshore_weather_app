aa444f70847d2760a3827aeb76c64e60ce8acb8b
Suman Adhikari <expertxkid@gmail.com>
2023-04-02 11:28:07 +0545

    refactor/PWA-1: add snack bars when fetching another city data in home_mobile.dart

--- lib/features/home/presentation/widgets/pages/home_mobile.dart
+++ lib/features/home/presentation/widgets/pages/home_mobile.dart
@@ -1,7 +1,32 @@
 part of 'home.dart';
 
-class HomeMobileWidget extends StatelessWidget {
+class HomeMobileWidget extends StatefulWidget {
   const HomeMobileWidget({Key? key}) : super(key: key);
+
+  @override
+  State<HomeMobileWidget> createState() => _HomeMobileWidgetState();
+}
+
+class _HomeMobileWidgetState extends State<HomeMobileWidget> {
+  @override
+  void initState() {
+    super.initState();
+
+    final forecastNotifier =
+        Provider.of<DailyForecastNotifier>(context, listen: false);
+
+    forecastNotifier.addListener(_forecastListener);
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+
+    final forecastNotifier =
+        Provider.of<DailyForecastNotifier>(context, listen: false);
+
+    forecastNotifier.removeListener(_forecastListener);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,44 +39,213 @@ class HomeMobileWidget extends StatelessWidget {
       WeatherForecastTile(),
       WeatherForecastTile(),
       WeatherForecastTile(),
+      WeatherForecastTile(),
     ];
 
-    return Consumer<DailyForecastNotifier>(
-      builder: (context, dailyForecastNotifier, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Row(
-              children: [
-                Image.asset(
-                  IconAssets.logo,
-                  width: 150,
-                ),
-                const SizedBox(width: 4),
-                Flexible(
-                  child: Text(
-                    dailyForecastNotifier.city,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: GoogleFonts.roboto(fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              IconAssets.logo,
+              width: 150,
             ),
-            actions: [
-              IconButton(
-                onPressed: () async {},
-                icon: const Icon(Icons.more_vert_rounded),
-              )
+            const SizedBox(width: 4),
+            Flexible(
+              child: Text(
+                context.watch<DailyForecastNotifier>().city,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: GoogleFonts.roboto(fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+        actions: [
+          PopupMenuButton<int>(
+            itemBuilder: (context) => <PopupMenuEntry<int>>[
+              const PopupMenuItem<int>(
+                value: 1,
+                child: Text('Change City'),
+              ),
             ],
+            onSelected: (selectedIndex) {
+              if (selectedIndex == 1) {
+                showChangeCityForm(context: context);
+              }
+            },
           ),
-          body: ListView.separated(
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) => mWeatherForecasts[index],
-            separatorBuilder: (context, index) => const SizedBox.shrink(),
-            itemCount: mWeatherForecasts.length,
-          ),
-        );
-      },
+        ],
+      ),
+      body: Consumer<DailyForecastNotifier>(
+        builder: (consumerContext, dailyForecastNotifier, child) {
+          return dailyForecastNotifier.state.maybeMap(
+              loading: (data) {
+                final oldForecasts = data.oldForecasts;
+
+                debugPrint("Loading called");
+
+                if (oldForecasts == null) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  return ListView.separated(
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (context, index) => mWeatherForecasts[index],
+                    separatorBuilder: (context, index) =>
+                        const SizedBox.shrink(),
+                    itemCount: oldForecasts.forecasts.length,
+                  );
+                }
+              },
+              loaded: (data) {
+                final forecasts = data.forecastResponse.forecasts;
+
+                return ListView.separated(
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) => mWeatherForecasts[index],
+                  separatorBuilder: (context, index) => const SizedBox.shrink(),
+                  itemCount: forecasts.length,
+                );
+              },
+              loadFailed: (failed) {
+                final oldForecasts = failed.oldForecasts;
+
+                if (oldForecasts == null) {
+                  return const Center(child: Text('Failed to load'));
+                } else {
+                  return ListView.separated(
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (context, index) => mWeatherForecasts[index],
+                    separatorBuilder: (context, index) =>
+                        const SizedBox.shrink(),
+                    itemCount: oldForecasts.forecasts.length,
+                  );
+                }
+              },
+              orElse: () => const Center(child: CircularProgressIndicator()));
+        },
+      ),
     );
   }
+
+  void _forecastListener() {
+    final forecastResponse = context.read<DailyForecastNotifier>().state;
+
+    forecastResponse.maybeMap(
+        loading: (data) {
+          if (data.oldForecasts != null) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content: Text('Fetching weather data..'),
+            ));
+          }
+        },
+        loaded: (_) {
+          ScaffoldMessenger.of(context).hideCurrentSnackBar();
+        },
+        loadFailed: (failed) {
+          if (failed.oldForecasts != null) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: const Text(
+                  'No such city exists. Please change the city and try again.'),
+              action: SnackBarAction(
+                  label: 'Change City',
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+                    showChangeCityForm(context: context);
+                  }),
+            ));
+          }
+        },
+        orElse: () {});
+  }
+}
+
+void showChangeCityForm({required BuildContext context}) {
+  final TextEditingController cityController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  showModalBottomSheet(
+    isScrollControlled: true,
+    context: context,
+    backgroundColor: Colors.transparent,
+    clipBehavior: Clip.hardEdge,
+    builder: (modalContext) {
+      return Container(
+          padding: MediaQuery.of(context).viewInsets,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(8),
+              topRight: Radius.circular(8),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Form(
+                  key: formKey,
+                  child: TextFormField(
+                    controller: cityController,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      if (value == null) {
+                        return 'City name cannot be empty';
+                      } else if (value.trim().length < 4) {
+                        return 'City name must be minimum 4 characters.';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Enter city name',
+                      errorMaxLines: 3,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.red,
+                        ),
+                        child: const Text('Cancel')),
+                    TextButton(
+                        onPressed: () {
+                          bool isFormValid =
+                              formKey.currentState?.validate() ?? false;
+
+                          if (!isFormValid) {
+                            return;
+                          }
+
+                          final city = cityController.text;
+
+                          context
+                              .read<DailyForecastNotifier>()
+                              .updateCity(city);
+
+                          Navigator.of(context).pop();
+                        },
+                        style: TextButton.styleFrom(
+                          foregroundColor: AppPalette.sunshineBlue,
+                        ),
+                        child: const Text('Set')),
+                  ],
+                ),
+              ],
+            ),
+          ));
+    },
+  );
 }
